ef34067452877f9d44279f1c6f580d23766e232d
lukenc <892821031@qq.com>
2024-04-22 10:59:02 +0800

    代码评分
    1、修复选择模型失效的问题
    2、支持百度大模型

--- src/main/java/com/wmsay/gpt4_lll/ScoreAction.java
+++ src/main/java/com/wmsay/gpt4_lll/ScoreAction.java
@@ -12,9 +12,11 @@ import com.wmsay.gpt4_lll.component.Gpt4lllTextArea;
 import com.wmsay.gpt4_lll.component.Gpt4lllTextAreaKey;
 import com.wmsay.gpt4_lll.model.ChatContent;
 import com.wmsay.gpt4_lll.model.Message;
+import com.wmsay.gpt4_lll.utils.ChatUtils;
 import com.wmsay.gpt4_lll.utils.CodeUtils;
 import com.wmsay.gpt4_lll.utils.CommonUtil;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import static com.wmsay.gpt4_lll.GenerateAction.chatHistory;
@@ -40,6 +42,7 @@ public class ScoreAction extends AnAction {
             }
         }
         String model = "gpt-3.5-turbo";
+        model = ChatUtils.getModelName(toolWindow);
         String replyLanguage = CommonUtil.getSystemLanguage();
         Project project = e.getProject();
         if (project != null) {
@@ -48,7 +51,11 @@ public class ScoreAction extends AnAction {
             SelectionModel selectionModel = editor.getSelectionModel();
             String selectedText = selectionModel.getSelectedText();
             Message systemMessage = new Message();
-            systemMessage.setRole("system");
+            if (model.contains("baidu")){
+                systemMessage.setRole("user");
+            }else {
+                systemMessage.setRole("system");
+            }
             systemMessage.setName("owner");
             systemMessage.setContent("你是一个计算机科学家，数据专家，有着多年的代码重构经验和多年的代码优化经验的架构师。你对代码的审查十分严格。对于审查评价代码，你总能指出代码中的各种问题，但你不会说不存在的问题，同时也不会遗漏任何一处问题。");
 
@@ -72,10 +79,10 @@ public class ScoreAction extends AnAction {
                 codeMessage.setContent("认真对每一项打分，以及总体得分，请开始评估以下代码，：\n"+selectedText);
 
                 ChatContent chatContent = new ChatContent();
-                chatContent.setMessages(List.of(systemMessage,message,codeMessage));
+                chatContent.setMessages(new ArrayList<>(List.of(systemMessage,message,codeMessage)));
                 chatContent.setModel(model);
                 chatContent.setTemperature(0.1);
-                chatHistory.addAll(List.of(systemMessage,message,codeMessage));
+                chatHistory.addAll(chatContent.getMessages());
 
                 //清理界面
                 Gpt4lllTextArea textArea= project.getUserData(Gpt4lllTextAreaKey.GPT_4_LLL_TEXT_AREA);
