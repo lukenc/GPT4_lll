0dcc52cf82a29f098f6135f4e6185b881275727e
lukenc <892821031@qq.com>
2025-01-25 11:46:11 +0800

    refactor: 重构`ScoreAction`类的`actionPerformed`方法
    - 增加了对项目状态的检查，确保在另一个任务运行时不会启动新的评分任务。
    - 更新了获取和处理聊天历史记录的方法，使用`ChatUtils`来管理项目相关的聊天数据。
    - 添加了对编辑器是否打开的验证，防止在没有打开文件的情况下尝试执行操作。
    - 修改了消息设置逻辑，使角色设定更加合理，并移除了不必要的名称设置。
    - 在整个操作完成后，更新了项目的运行状态以表示任务完成。

--- src/main/java/com/wmsay/gpt4_lll/ScoreAction.java
+++ src/main/java/com/wmsay/gpt4_lll/ScoreAction.java
@@ -6,30 +6,41 @@ import com.intellij.openapi.actionSystem.CommonDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.SelectionModel;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowManager;
 import com.wmsay.gpt4_lll.component.Gpt4lllTextArea;
-import com.wmsay.gpt4_lll.component.Gpt4lllTextAreaKey;
+import com.wmsay.gpt4_lll.model.key.Gpt4lllTextAreaKey;
 import com.wmsay.gpt4_lll.model.ChatContent;
 import com.wmsay.gpt4_lll.model.Message;
 import com.wmsay.gpt4_lll.model.enums.ProviderNameEnum;
 import com.wmsay.gpt4_lll.utils.ChatUtils;
 import com.wmsay.gpt4_lll.utils.CodeUtils;
 import com.wmsay.gpt4_lll.utils.CommonUtil;
+import com.wmsay.gpt4_lll.utils.ModelUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.wmsay.gpt4_lll.GenerateAction.chatHistory;
-import static com.wmsay.gpt4_lll.GenerateAction.nowTopic;
 
 public class ScoreAction extends AnAction {
 
     @Override
     public void actionPerformed(AnActionEvent e) {
-        if (chatHistory != null && !chatHistory.isEmpty() && !nowTopic.isEmpty()) {
-            JsonStorage.saveConservation(nowTopic, chatHistory);
-            chatHistory.clear();
+        Project project = e.getProject();
+        if (project==null){
+            Messages.showMessageDialog(e.getProject(), "不是一个项目/no project here", "Error", Messages.getErrorIcon());
+            return;
+        }
+        if(CommonUtil.isRunningStatus(project)){
+            Messages.showMessageDialog(project, "Please wait, another task is running", "Error", Messages.getErrorIcon());
+            return;
+        }else {
+            CommonUtil.startRunningStatus(project);
+        }
+        if (ChatUtils.getProjectChatHistory(project) != null && !ChatUtils.getProjectChatHistory(project).isEmpty() && !ChatUtils.getProjectTopic(project).isEmpty()) {
+            JsonStorage.saveConservation(ChatUtils.getProjectTopic(project), ChatUtils.getProjectChatHistory(project));
+            ChatUtils.getProjectChatHistory(project).clear();
         }
         ToolWindowManager toolWindowManager = ToolWindowManager.getInstance(e.getProject());
         ToolWindow toolWindow = toolWindowManager.getToolWindow("GPT4_lll");
@@ -43,31 +54,34 @@ public class ScoreAction extends AnAction {
             }
         }
         String model = "gpt-3.5-turbo";
-        model = ChatUtils.getModelName();
+        model = ChatUtils.getModelName(e.getProject());
         String replyLanguage = CommonUtil.getSystemLanguage();
-        Project project = e.getProject();
         if (project != null) {
-            Editor editor = e.getRequiredData(CommonDataKeys.EDITOR);
+            Editor editor = e.getData(CommonDataKeys.EDITOR);
+            if (editor==null){
+                Messages.showMessageDialog(project, "Editor is not open. Please open the file that you want to do something", "Error", Messages.getErrorIcon());
+                CommonUtil.stopRunningStatus(project);
+                return;
+            }
             String fileType = CommonUtil.getOpenFileType(project);
             SelectionModel selectionModel = editor.getSelectionModel();
             String selectedText = selectionModel.getSelectedText();
             Message systemMessage = new Message();
-            if (ProviderNameEnum.BAIDU.getProviderName().equals(WindowTool.getSelectedProvider())){
+            if (ProviderNameEnum.BAIDU.getProviderName().equals(ModelUtils.getSelectedProvider(project))){
                 systemMessage.setRole("user");
             }else {
                 systemMessage.setRole("system");
             }
-            systemMessage.setName("owner");
+//            systemMessage.setName("owner");
             systemMessage.setContent("你是一个计算机科学家，数据专家，有着多年的代码重构经验和多年的代码优化经验的架构师。你对代码的审查十分严格。对于审查评价代码，你总能指出代码中的各种问题，但你不会说不存在的问题，同时也不会遗漏任何一处问题。");
 
             Message message = new Message();
             if (selectedText != null) {
-                chatHistory.clear();
+                ChatUtils.getProjectChatHistory(project).clear();
                 selectedText = selectedText.trim();
-                nowTopic = CommonUtil.generateTopicByMethodAndTime(selectedText, "Score");
+                ChatUtils.setProjectTopic(project,CommonUtil.generateTopicByMethodAndTime(selectedText, "Score"));
 
                 message.setRole("user");
-                message.setName("owner");
                 //todo 通过fileType的筛选 确定message的Content，如果fileType中存在TypeScript或者JavaScript，忽略大小写，则message.setContent("1")
                 if ("Vue".equalsIgnoreCase(fileType) || "TypeScript".equalsIgnoreCase(fileType) || "JavaScript".equalsIgnoreCase(fileType) || (fileType != null && fileType.toLowerCase().contains("javascript")) || (fileType != null && fileType.toLowerCase().contains("typescript"))) {
                     message.setContent("评估不限于以下方面：" + CodeUtils.WEB_DEV_STD + "。如果该评估总分是100，帮忙使用" + replyLanguage + "语言，评估下面的" + fileType + "代码的得分，一定要确保评估的准确性，接下来我将给你需要评估的代码。");
@@ -80,10 +94,10 @@ public class ScoreAction extends AnAction {
                 codeMessage.setContent("认真对每一项打分，以及总体得分，请开始评估以下代码，：\n"+selectedText);
 
                 ChatContent chatContent = new ChatContent();
-                chatContent.setMessages(new ArrayList<>(List.of(systemMessage,message,codeMessage)));
+                chatContent.setMessages(new ArrayList<>(List.of(systemMessage,message,codeMessage)),ModelUtils.getSelectedProvider(project));
                 chatContent.setModel(model);
                 chatContent.setTemperature(0.1);
-                chatHistory.addAll(chatContent.getMessages());
+                ChatUtils.getProjectChatHistory(project).addAll(chatContent.getMessages());
 
                 //清理界面
                 Gpt4lllTextArea textArea= project.getUserData(Gpt4lllTextAreaKey.GPT_4_LLL_TEXT_AREA);
@@ -93,6 +107,7 @@ public class ScoreAction extends AnAction {
                 new Thread(() -> GenerateAction.chat(chatContent, project, false,true,"")).start();
             }
         }
+        CommonUtil.stopRunningStatus(project);
         // TODO: insert action logic here
     }
 }
