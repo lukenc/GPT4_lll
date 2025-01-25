3fc2069fbb65009dc3f833a863798558433ed9d4
lukenc <892821031@qq.com>
2025-01-25 11:49:00 +0800

    refactor(model): 重构 Gpt4lllTextAreaKey 类的包结构
    - 将 Gpt4lllTextAreaKey 类从 component包移动到 model/key 包- 更新包声明和导入语句

--- src/main/java/com/wmsay/gpt4_lll/model/key/Gpt4lllTextAreaKey.java
+++ src/main/java/com/wmsay/gpt4_lll/model/key/Gpt4lllTextAreaKey.java
@@ -1,6 +1,7 @@
-package com.wmsay.gpt4_lll.component;
+package com.wmsay.gpt4_lll.model.key;
 
 import com.intellij.openapi.util.Key;
+import com.wmsay.gpt4_lll.component.Gpt4lllTextArea;
 
 public class Gpt4lllTextAreaKey {
     public static final Key<Gpt4lllTextArea> GPT_4_LLL_TEXT_AREA = Key.create("GPT4lllTextArea");
