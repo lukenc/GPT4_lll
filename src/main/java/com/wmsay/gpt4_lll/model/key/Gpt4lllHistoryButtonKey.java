832dd43c9c25a70555e4a53ce59d780a23f33e87
lukenc <892821031@qq.com>
2025-01-25 11:33:00 +0800

    feat: 添加`Gpt4lllHistoryButtonKey`类及按键常量
    - 新增`Gpt4lllHistoryButtonKey`类位于`com.wmsay.gpt4_lll.model.key`包中。
    - 定义了静态最终键值`GPT_4_LLL_HISTORY_BUTTON`，用于标识历史记录按钮。

---
+++ src/main/java/com/wmsay/gpt4_lll/model/key/Gpt4lllHistoryButtonKey.java
@@ -0,0 +1,10 @@
+package com.wmsay.gpt4_lll.model.key;
+
+import com.intellij.openapi.util.Key;
+import com.wmsay.gpt4_lll.component.Gpt4lllTextArea;
+
+import javax.swing.*;
+
+public class Gpt4lllHistoryButtonKey {
+    public static final Key<JButton> GPT_4_LLL_HISTORY_BUTTON = Key.create("GPT4lllHistoryButton");
+}
