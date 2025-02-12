ceeca3b267e4c6251424055f2a7ffef255cb7492
lukenc <892821031@qq.com>
2025-02-12 16:53:25 +0800

    feat(model): 在 Delta 类中添加 reasoningContent 字段
    - 在 Delta 类中新增 reasoningContent 字段，用于存储推理内容

--- src/main/java/com/wmsay/gpt4_lll/model/Delta.java
+++ src/main/java/com/wmsay/gpt4_lll/model/Delta.java
@@ -3,6 +3,7 @@ package com.wmsay.gpt4_lll.model;
 
 import javax.annotation.Generated;
 import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
 @Generated("net.hexar.json2pojo")
 @SuppressWarnings("unused")
@@ -10,6 +11,8 @@ public class Delta {
 
     @Expose
     private String content;
+    @SerializedName("reasoning_content")
+    private String reasoningContent;
 
     public String getContent() {
         return content;
@@ -19,4 +22,11 @@ public class Delta {
         this.content = content;
     }
 
+    public String getReasoningContent() {
+        return reasoningContent;
+    }
+
+    public void setReasoningContent(String reasoningContent) {
+        this.reasoningContent = reasoningContent;
+    }
 }
